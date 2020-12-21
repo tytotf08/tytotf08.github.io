200db7d85f52f25f5dbda9752ece81c059564a4a
tytotf08 <joshleeliu@gmail.com>
2020-12-21 14:01:19 -0500

    Add testing Chrome.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
 "use strict";
+const userAgent = navigator.userAgent.toLowerCase();
+if (userAgent.indexOf("chrome") > -1) {
+	const comment = document.createComment("running in chrome");
+	document.body.insertBefore(comment, document.body.childNodes[0]);
+}
