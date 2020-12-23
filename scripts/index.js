85dec10f5f06b5c391d851618b4fb22a135b3409
tytotf08 <joshleeliu@gmail.com>
2020-12-23 12:27:00 -0500

    Well, more.

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,7 @@
 	if (navigator.userAgent.toLowerCase().indexOf("chrome") > -1) {
 		document.body.style.fontFamily = "sans-serif";
 	}
+	document.querySelectorAll("img").forEach((el) => {
+		el.setAttribute("alt", "404 - image not found");
+	});
 })();
